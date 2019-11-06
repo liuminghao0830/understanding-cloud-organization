a6c2153698bd70e73aa00605192d4d4f9dd23f46
Minghao <liuminghao0830@gmail.com>
2019-11-06 14:10:07 +0800

    ensemble 3 models

--- predict.py
+++ predict.py
@@ -1,50 +1,50 @@
-import keras
+import keras, cv2
 import pandas as pd
 from tta_wrapper import tta_segmentation
 import segmentation_models as sm
 from data import DataGenerator
+from utils import post_process, mask2rle
 
 sub_df = pd.read_csv('../data/sample_submission.csv')
 sub_df['ImageId'] = sub_df['Image_Label'].apply(lambda x: x.split('_')[0])
 test_imgs = pd.DataFrame(sub_df['ImageId'].unique(), columns=['ImageId'])
 
 # # Predict on Test Set
-model = sm.Unet('resnet18', classes=4, input_shape=(320, 480, 3), activation='sigmoid')
-model.load_weights('../resnet18_unet.h5')
+model1 = sm.Unet('efficientnetb4', classes=4, input_shape=(320, 480, 3), activation='sigmoid')
+model1.load_weights('../efn_unet.h5')
+model2 = sm.Unet('resnet18', classes=4, input_shape=(320, 480, 3), activation='sigmoid')
+model2.load_weights('../resnet18_unet.h5')
+model3 = sm.Unet('densenet121', classes=4, input_shape=(320, 480, 3), activation='sigmoid')
+model3.load_weights('../dense_unet.h5')
 
-model = tta_segmentation(model, h_flip=True, h_shift=(-10, 10), merge='mean')
+model1 = tta_segmentation(model1, h_flip=True, h_shift=(-10, 10), merge='mean')
+model2 = tta_segmentation(model2, h_flip=True, h_shift=(-10, 10), merge='mean')
+model3 = tta_segmentation(model3, h_flip=True, h_shift=(-10, 10), merge='mean')
 
 threshold = 0.45
-min_size = [15000, 15000, 15000, 15000]
+#min_size = [15000, 15000, 15000, 15000]
+min_size = [20000, 20000, 22500, 10000]
 
 test_df = []
 encoded_pixels = []
-TEST_BATCH_SIZE = 100
+BATCH_SIZE = 1
 
-for i in range(0, test_imgs.shape[0], TEST_BATCH_SIZE):
-    batch_idx = list(
-        range(i, min(test_imgs.shape[0], i + TEST_BATCH_SIZE))
-    )
+test_generator = DataGenerator(test_imgs.index, df=test_imgs, shuffle=False, mode='predict',
+                                   dim=(350, 525), reshape=(320, 480), n_channels=3, 
+                                   gamma=0.8, base_path='../data/test_images', 
+                                   target_df=sub_df, batch_size=BATCH_SIZE, n_classes=4)
 
-    test_generator = DataGenerator(
-        batch_idx,
-        df=test_imgs,
-        shuffle=False,
-        mode='predict',
-        dim=(350, 525),
-        reshape=(320, 480),
-        n_channels=3,
-        gamma=0.8,
-        base_path='../data/test_images',
-        target_df=sub_df,
-        batch_size=1,
-        n_classes=4)
 
-    batch_pred_masks = model.predict_generator(
-        test_generator, 
-        workers=1,
-        verbose=1
-    ) 
+for i, x_batch in enumerate(test_generator):
+    if i % 100 == 0:
+        print("Predicting {}th batch".format(i))
+    
+    batch_idx = list(range(i*BATCH_SIZE, min(test_imgs.shape[0], i*BATCH_SIZE + BATCH_SIZE)))
+    
+    batch_pred_masks1 = model1.predict(x_batch)
+    batch_pred_masks2 = model2.predict(x_batch)
+    batch_pred_masks3 = model3.predict(x_batch)
+    batch_pred_masks = (batch_pred_masks1+batch_pred_masks2+batch_pred_masks3) / 3
     # Predict out put shape is (320X480X4)
     # 4  = 4 classes, Fish, Flower, Gravel Surger.
     
@@ -59,7 +59,8 @@ for i in range(0, test_imgs.shape[0], TEST_BATCH_SIZE):
             pred_mask = pred_masks[...,k].astype('float32') 
             
             if pred_mask.shape != (350, 525):
-                pred_mask = cv2.resize(pred_mask, dsize=(525, 350), interpolation=cv2.INTER_LINEAR)
+                pred_mask = cv2.resize(pred_mask, dsize=(525, 350), 
+                                        interpolation=cv2.INTER_LINEAR)
                 
             pred_mask, num_predict = post_process(pred_mask, threshold, 
                                                   min_size[k], (350, 525))
